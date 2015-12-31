b7bd14e2ad0fb94cbc245150be66f5b6b5753ebb
Chris Rossi <chris@archimedeanco.com>
2015-12-31 14:30:50 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = '0.1dev'
+
+requires = [
+]
+tests_require = requires + []
+
+setup(name='dip2svg',
+      version=VERSION,
+      description='Prettier Schematics for DipTrace',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      entry_points="""\
+      [console_scripts]
+      dip2svg = dip2svg.main:main
+      """)
